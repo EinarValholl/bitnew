5606869d33e0361b21aad9b4129dda56fd57894d
Valholl Einar <kachorovskiyvi@gmail.com>
2017-02-03 00:14:50 +0200

    Create me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MeComponent } from './me/me/me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent
   ],
   imports: [
     BrowserModule,
